1db18f13b0be8e3f09b2efe090394e22be894dba
Gavin Barnett <gavbarnett@gmail.com>
2018-07-04 18:48:58 +0100

    added minor scale, now plays a tune (kinda)
    its a bit rubbish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,64 @@
-var timeBase = 500
+var timeBase = 100
 
 //cross browser support
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 function playMusic(){
-    var OSC1 = new OSC(440, 0.0001,0.2)
-    var composer1 = new composer(OSC1)
-    OSC1.begin()
-    var t = setInterval(composer1.nextNote,timeBase);
+    var composer1 = new composer()
+    var t = setInterval(composer1.nextSection,1000);
 }
 
 function composer(OSCin){
-    var myOSC = OSCin
+    var myOSC = new OSC(440, 0.0001,0.2)
     var previousNote = 220
     var previousBeat = 0
-    var baseNote = 220
+    var baseNote = 35 //49 = A
+    var majorChord = []
     var melody = []
     var rhythm = []
+    var volume = []
     var m = 0
-    for (var i = 0; i <8; i++){
-        melody[i] = baseNote * (Math.round(1+Math.random()*3))/(1+Math.round(Math.random()*3))
-        rhythm[i] = 4/(Math.pow(2,Math.round(Math.random()*8)))
+    // Initialize Minor chord
+    majorChord[0] = 0
+    majorChord[1] = 2
+    majorChord[2] = 3
+    majorChord[3] = 5
+    majorChord[4] = 7
+    majorChord[5] = 8
+    majorChord[6] = 10
+    majorChord[7] = 12
+    // Initialize melody & rhythm
+    melody[0] = 0
+    rhythm[0] = Math.round(Math.random()*7)+1
+    volume[0] = 0.2
+    for (var i = 1; i <16; i++){
+        melody[i] = majorChord[i%7]
+        rhythm[i] = Math.round(Math.random()*7)+1/2
+        volume[i] = 0.2*Math.round(Math.random()*1.2)
     }
-    console.log(melody)
-    this.nextNote =function(){
-        if (previousBeat<=1){
-            var nextNote = melody[m]
-            nextNote = Math.min(nextNote,12000)
-            nextNote = Math.max(nextNote,40)
-            nextBeat = rhythm[m]
-            myOSC.tone(nextNote,nextBeat,0.2)//*Math.round(Math.random()))
-            previousBeat = nextBeat
-            previousNote = nextNote
-           // console.log(nextNote) 
-            m++
-            if (m==8){m=0}
+    var timeStamp = myOSC.begin()
+    this.nextSection =function(){
+        if (timeStamp<10){
+                for (var i = 0; i <16; i++){
+                    timeStamp = myOSC.tone(Math.pow(2,((baseNote + melody[i]-49)/12))*440,rhythm[i],volume[i])    
+                } 
+                for (var i = 0; i <16; i++){
+                    if (Math.random()>0.9){
+                        melody[i] = majorChord[Math.round(Math.random()*7)]
+                    }
+                    if (Math.random()>0.9){
+                        rhythm[i]  = Math.round(Math.random()*3)+1
+                    }
+                    if (Math.random()>0.9){
+                        //volume[i] = 0.2*Math.round(Math.random()*1.2)
+                    }
+                }
+              
+                if (Math.random()>0.7){
+                    baseNote = Math.min(59,baseNote + majorChord[5]-Math.round(Math.random())*12)
+                }
         }else{
-            previousBeat=previousBeat-1
+            timeStamp -= 1
         }
     }
 }
@@ -44,19 +66,23 @@ function composer(OSCin){
 function OSC(tone, beats, volume){
     oscillator = audioCtx.createOscillator();
     gainNode = audioCtx.createGain();
-    oscillator.type = 'sawtooth';
+    oscillator.type = 'triangle';
     oscillator.frequency.setValueAtTime(tone, audioCtx.currentTime); // value in hertz
     oscillator.connect(gainNode);
     gainNode.gain.setValueAtTime(volume, audioCtx.currentTime);
     gainNode.connect(audioCtx.destination);
+    var timeStamp = 0;
     this.begin = function(){
         oscillator.start();
+        timeStamp = audioCtx.currentTime
+        return(timeStamp)
     }
     this.tone = function(tone,beats,volume){
-        var now = audioCtx.currentTime
-        gainNode.gain.setValueAtTime(volume, now);
-        oscillator.frequency.setValueAtTime(tone, now);
-        gainNode.gain.setTargetAtTime(0, now+beats*timeBase-0.015, 0.015);
+        gainNode.gain.setValueAtTime(volume, timeStamp);
+        oscillator.frequency.setValueAtTime(tone, timeStamp);
+        timeStamp += (beats*timeBase/1000)-0.015
+        gainNode.gain.setTargetAtTime(0, timeStamp, 0.015);
+        return (timeStamp-audioCtx.currentTime)
     }
 
 }
